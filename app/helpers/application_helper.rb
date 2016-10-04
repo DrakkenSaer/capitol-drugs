4c5399106e8a8b83fecbb4907a3baa788b46e68e
drakkensaer <shadowdrakkeen@gmail.com>
2016-10-04 21:15:33 +0000

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,8 @@ module ApplicationHelper
         content_for :title, page_title.to_s
     end
     
+    # Usage: <%= user_email_or_name(@user) %>
     def user_email_or_name(user)
-       user.firstname? ? user.firstname : user.email 
+       user.bill_address ? user.bill_address.firstname : user.email 
     end
 end
