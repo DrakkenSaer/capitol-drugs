475a9a0f8eb24cda4bca6c12d14504d3ee9bb079
drakkensaer <shadowdrakkeen@gmail.com>
2017-04-18 03:39:42 +0000

    update on import algorithm

--- sorting_function.rb
+++ sorting_function.rb
@@ -3,17 +3,20 @@ sheet = xls.sheet(0)
 product_sheet_hash_array = sheet.each(name: 'Name/Item_Size/Form', sku: 'CDI_Sku', price: 'Everyday_Price', upc: 'UPC_Code')
 
 # Create Taxons
-sheet.each_with_index(brand: 'Brand', categories: 'Category', sub_category: 'Sub_Category') do |hash, index|
+sheet.each_with_index(brand: 'Brand', category: 'Category', sub_category: 'Sub_Category') do |hash, index|
     next if index == 0
     hash.map do |key, value|
-        puts hash, key, value
-
         query = nil
-        if key.to_s == 'sub_category'
-            category = Spree::Taxon.find_by(name: 'Categories')
-            query = { name: hash[:categories].to_s.titleize, parent_id: category.id, taxonomy_id: category.taxonomy_id }
+        taxonomy_name = key.to_s.gsub('sub_', '')
+        taxonomy = Spree::Taxonomy.find_or_create_by!(name: taxonomy_name.titleize)
+
+        if key.to_s.include? 'sub_'
+            taxon = Spree::Taxon.find_by_name(taxonomy.name)
+            query = { name: hash[taxonomy_name.to_sym].to_s.titleize,
+                      parent_id: taxon.id,
+                      taxonomy_id: taxonomy.id }
         else
-            query = { name: key.to_s.titleize }
+            query = { name: taxonomy.name }
         end
 
         parent = Spree::Taxon.find_or_create_by!(query)
@@ -22,4 +25,3 @@ sheet.each_with_index(brand: 'Brand', categories: 'Category', sub_category: 'Sub
                                         taxonomy_id: parent.taxonomy_id) unless value.to_s.titleize == parent.name
     end
 end
-
