2b88254a9b1843715e4942c18c8424c6355baeb5
Lilith Saer <lilith.saer@cayuse.com>
2018-01-23 22:35:51 -0800

    include utils

--- app/controllers/api/v1/import_controller.rb
+++ app/controllers/api/v1/import_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::ImportController < ApiController
   protect_from_forgery with: :null_session
 
-  importUtil = Utils::ImportUtil.new
-
   def csv
-    logger.debug "#{params}"
+    importUtil = Utils::ImportUtil.new
 
     if importUtil.import_csv(params[:spreadsheet])
       render plain: "OK"
