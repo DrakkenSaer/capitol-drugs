3b99f0c9f28b3a6dcdcaef16490c4b5adc06295a
Lilith Saer <lilith.saer@cayuse.com>
2018-01-23 22:16:10 -0800

    initialize class

--- app/controllers/api/v1/import_controller.rb
+++ app/controllers/api/v1/import_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ImportController < ApiController
   protect_from_forgery with: :null_session
 
-  importUtil = Utils::ImportUtil.new
+  importUtil = ::Utils::ImportUtil.new
 
   def csv
     logger.debug "#{params}"
